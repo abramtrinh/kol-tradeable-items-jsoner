9db34653f292423aae0234d93cbf27467ea2bda1
Abram Trinh <ahtrinh@ucsd.edu>
2023-01-19 19:18:52 -0800

    Working implementation of JSONer using colly.

---
+++ README.md
@@ -0,0 +1,41 @@
+# kol-tradeable-items-jsoner
+
+> Processes tradeable item data to JSON format.
+
+This tool uses [Colly](https://github.com/gocolly/colly).
+
+###  Example
+```shell
+
+```
+
+
+
+## The main problem:
+If you wanted the item name, item number, and tradeability, you would have to visit each item's wiki url to get that data.
+
+This means crawling and scraping through ~12000 items URLs. (The number of items constantly increase.)
+
+The only issue with this is that the site is already pretty laggy. Adding more traffic would be bad manners to the site and the players.
+
+## The solution:
+There is a dropdown for tradeable items on a different site. 
+
+I can just get the HTML file and then parse the item name and item number from it.
+
+The tradability of the item is already guaranteed by the fact that it has trade data for it.
+
+After doing some data processing, the item name and item number are encoded into a JSON file.
+
+## Reasoning:
+I didn't want to unnecessarily get a new HTML file every time the program is run.
+
+I chose to scrape data from a local HTML file instead of the website because the data is static.
+
+## Note:
+First time users need to run with "-u" flag to generate the HTML file from the website locally.
+
+After that users can just run without the "-u" flag since HTML file exists locally.
+
+Users can rerun with "-u" flag if they need to re-update the HTML file (if new tradeable items are created).
+
---
+++ itemJSONer.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"regexp"
+
+	"github.com/gocolly/colly/v2"
+)
+
+const URL = "https://kol.coldfront.net/newmarket/"
+const htmlFile = "newmarket.html"
+const htmlFilePath = "file://./" + htmlFile
+
+var updateFileFlag = flag.Bool("u", false, "bool to get/update HTML file from URL")
+
+type item struct {
+	Name string `json:"name"`
+	ID   string `json:"itemid"`
+}
+
+func main() {
+	flag.Parse()
+
+	if *updateFileFlag {
+		fmt.Printf("Flag passed %t\n", *updateFileFlag)
+		if err := getURLToFile(); err != nil {
+			fmt.Printf("error occured with getURLToFile: %v", err)
+			return
+		}
+	}
+
+	if _, err := os.Stat(htmlFile); err != nil {
+		fmt.Printf("%s does not exist, please run with the -u flag to initialize.\n", htmlFile)
+		return
+	}
+
+	//Allows you to simulate local file as HTTP request
+	//Below, "." local directory is chosen and transport is hooked up to colly
+	transport := &http.Transport{}
+	transport.RegisterProtocol("file", http.NewFileTransport(http.Dir(".")))
+
+	collector := colly.NewCollector()
+	collector.WithTransport(transport)
+
+	var itemList []item
+
+	//Works on <select name=itemlist first then <option
+	collector.OnHTML("select[name=itemlist] option", func(h *colly.HTMLElement) {
+		//This returns the value attribute of <option value=""
+		urlString := h.Attr("value")
+
+		idStringNumber, err := parseItemNumber(urlString)
+		if err != nil {
+			fmt.Printf("error parsing url: %v", err)
+			return
+		}
+
+		newItem := item{
+			//This returns the text inbetween <option></option>
+			Name: h.Text,
+			ID:   idStringNumber,
+		}
+		itemList = append(itemList, newItem)
+	})
+
+	collector.Visit(htmlFilePath)
+
+	//JSONify the data I got.
+	content, err := json.Marshal(itemList)
+	if err != nil {
+		fmt.Printf("error marshalling json: %v", err)
+		return
+	}
+
+	os.WriteFile("items.json", content, 0644)
+	fmt.Printf("Wrote item data to %s", htmlFile)
+}
+
+// Gets html page and locally stores it in working directory.
+func getURLToFile() error {
+	fmt.Printf("Beginning to get data from %s\n", URL)
+	// Requesting HTML page
+	resp, err := http.Get(URL)
+	if err != nil {
+		return fmt.Errorf("failed getting URL: %w", err)
+	}
+	defer resp.Body.Close()
+
+	outputFile, err := os.Create(htmlFile)
+	if err != nil {
+		return fmt.Errorf("failed creating file: %w", err)
+	}
+	defer outputFile.Close()
+
+	if _, err := io.Copy(outputFile, resp.Body); err != nil {
+		return fmt.Errorf("failed copying file content: %w", err)
+	}
+	fmt.Printf("Completed, data written to %s\n", htmlFile)
+	return nil
+}
+
+// Parses a url string to find the item number which is preceded by "itemid="
+func parseItemNumber(urlString string) (string, error) {
+	//Regex to find itemid=0000 where 0000 is any id number from urlString
+	reg, err := regexp.Compile(`itemid=(\d*)`)
+	if err != nil {
+		return "", fmt.Errorf("error compiling regex: %w", err)
+	}
+
+	//returns itemid=0000 slice where [0] is itemid=0000 and [1] is 0000
+	idStringNumber := reg.FindStringSubmatch(urlString)[1]
+
+	return idStringNumber, nil
+
+}
